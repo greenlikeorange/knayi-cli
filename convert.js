2f74855f9ddf7e484f66176b3bd3102f9b683177
Thuya Myo Nyunt <ty@kny.co>
2018-08-17 19:36:42 +0630

    Update new version methods, and update process for big text files without memory issue

--- convert.js
+++ convert.js
@@ -1,41 +1,62 @@
 #! /usr/bin/env node
 
 var fs = require("fs");
+var readline = require('readline');
 var knayi = require("knayi-myscript");
 
-var outputUnicode = function(outText) {
-  if (process.argv.length > 3) {
-    var outFile = process.argv[3];
-    fs.writeFile(outFile, outText, function(err) {
-      if (err) {
-        throw err;
-      } else {
-        console.log("Output to " + outFile);
-      }
-    });
+var outputWriteSteam;
+if (process.argv.length > 3) {
+  var outFile = process.argv[3];
+  outputWriteSteam = fs.createWriteStream(outFile);
+}
+
+var outputUnicode = function(outputLine, done) {
+  if (outputWriteSteam) {
+    outputWriteSteam.write(outputLine + '\n');
   } else {
-    console.log(outText);
+    console.log(outputLine);
   }
-};
+}
+
+outputUnicode.done = function(lastLine) {
+  if (outputWriteSteam) {
+    outputWriteSteam.end(lastLine);
+  } else {
+    console.log(lastLine); 
+  }
+}
 
 if (process.argv.length > 2) {
   var srcFile = process.argv[2];
-  fs.readFile(srcFile, {encoding: 'utf-8'}, function(err, srcText) {
-    if (err) {
-      throw err;
-    } else {
-      var k = knayi(srcText);
-      var myFont = k.getFontType();
-      if (myFont === "zawgyi") {
-        k.fontConvert("unicode5", function(unicoded) {
-          outputUnicode(unicoded);
-        });
-      } else {
-        console.log("You're already in Unicode format!");
-        outputUnicode(srcText);
-      }
-    }
+  var lineReader = readline.createInterface({
+    input: fs.createReadStream(srcFile),
   });
+
+  var fontType;
+  var lineCount = 0;
+  var lastLine = '';
+  
+  lineReader.on('line', function (currentLine) {
+    lineCount++
+    if (lineCount < 100) {
+      lastLine += currentLine + '\n';
+      return;
+    } else if (lineCount === 100) {
+      lastLine += currentLine;
+      fontType = knayi.fontDetect(first100);
+    }
+
+    if (fontType === 'zawgyi') {
+      outputUnicode(knayi.fontConvert(lastLine, 'unicode', 'zawgyi'));
+    } else {
+      outputUnicode(currentLine); 
+    }
+    lastLine = currentLine;
+  });
+
+  lineReader.on('close', function () {
+    outputUnicode.done(knayi.fontConvert(lastLine, 'unicode'));
+  })
 } else {
   console.log("Please provide a file name.");
 }
